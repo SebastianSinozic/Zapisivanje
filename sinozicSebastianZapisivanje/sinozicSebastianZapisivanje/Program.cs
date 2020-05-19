0948e867821140a99c7138b910f165ddbf58e635
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-05-19 17:19:23 +0200

    Add files via upload

---
+++ sinozicSebastianZapisivanje/sinozicSebastianZapisivanje/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinozicSebastianZapisivanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using(StreamWriter sw = new StreamWriter(@"C:\drivers\Corona.txt"))
+                {
+                sw.Write("Bazinga");
+                Console.WriteLine("File is created");
+            }
+        }
+    }
+}
